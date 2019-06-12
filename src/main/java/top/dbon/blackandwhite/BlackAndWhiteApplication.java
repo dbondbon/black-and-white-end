f1ba12b5a3641c94f456c15361b38102e51dd593
赵志才 <zhaozhicai@britecloudtech.com>
2019-06-12 21:02:00 +0800

    与前台交互

--- src/main/java/top/dbon/blackandwhite/BlackAndWhiteApplication.java
+++ src/main/java/top/dbon/blackandwhite/BlackAndWhiteApplication.java
@@ -3,9 +3,11 @@ package top.dbon.blackandwhite;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan("top.dbon.blackandwhite.mapper")
+@ComponentScan("top.dbon.blackandwhite.controller")
 public class BlackAndWhiteApplication {
 
 	public static void main(String[] args) {
