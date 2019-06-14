69a50f6a11815f052bc58bc4d8309ccc7fba1288
赵志才 <zhaozhicai@britecloudtech.com>
2019-06-14 16:03:36 +0800

    买书查询、商品详情后台

---
+++ src/main/java/top/dbon/blackandwhite/controller/GoodsController.java
@@ -0,0 +1,42 @@
+package top.dbon.blackandwhite.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import top.dbon.blackandwhite.domain.Goods;
+import top.dbon.blackandwhite.service.GoodsService;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/goods")
+public class GoodsController {
+
+  @Autowired
+  private GoodsService goodsService;
+
+  @PostMapping("/published")
+  @ResponseBody
+  public Map<String, Object> published(@RequestBody Goods goods) {
+    goodsService.insertGoods(goods);
+    HashMap<String, Object> map = new HashMap<>();
+    map.put("code", "0");
+    return map;
+  }
+
+  @PostMapping("/findGoods")
+  @ResponseBody
+  public Map<String, Object> defaultGoods(@RequestBody Goods goods) {
+    List<Goods> list = goodsService.selectListByUserIDAndGoodsName(goods);
+    HashMap<String, Object> map = new HashMap<>();
+    map.put("code", "0");
+    map.put("list",list);
+    return map;
+  }
+
+}
