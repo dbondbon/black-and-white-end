9afc1de5db10f211e3f010c2e456e1f50213e603
尘土 <1669691816@qq.com>
2019-06-25 13:27:37 +0800

    后台接收图片并将图片存储到指定文件夹下

--- src/main/java/top/dbon/blackandwhite/controller/UploadImgController.java
+++ src/main/java/top/dbon/blackandwhite/controller/UploadImgController.java
@@ -3,17 +3,47 @@ package top.dbon.blackandwhite.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import top.dbon.blackandwhite.common.AjaxResult;
+import top.dbon.blackandwhite.service.GoodsService;
+import top.dbon.blackandwhite.util.UUIDUtils;
 
+import java.io.*;
+
+/**
+ * 接收图片控制器
+ */
 @Controller
 @RequestMapping("/img")
 public class UploadImgController {
 
     @PostMapping("/bookImg")
     @ResponseBody
-    public AjaxResult uploadBookImg() {
+    public AjaxResult uploadBookImg(@RequestParam("file") MultipartFile file) {
+        BufferedOutputStream out = null;
+        try {
+            out = new BufferedOutputStream(new FileOutputStream(new File("G:\\black-and-white\\black-and-white-end\\img",
+                    UUIDUtils.getInstance().nextId() +
+                            file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf('.')))));
+            out.write(file.getBytes());
+            out.flush();
+            out.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return AjaxResult.error(1, "上传失败");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return AjaxResult.error(1, "上传失败");
+        } finally {
+            try {
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return AjaxResult.error(1, "上传失败");
+            }
+        }
         return AjaxResult.success();
     }
-
 }
