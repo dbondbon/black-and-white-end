1825c58476c9f24089cfaed5d29b6587f06b88f2
尘土 <1669691816@qq.com>
2019-07-07 10:02:36 +0800

    增加常量类

---
+++ src/main/java/top/dbon/blackandwhite/common/Constant.java
@@ -0,0 +1,17 @@
+package top.dbon.blackandwhite.common;
+
+/**
+ * @ClassName Constant
+ * @Descrcription 常量类
+ * @Author zzc
+ * @Date 2019/7/7 9:44
+ * @Version 1.0
+ */
+public class Constant {
+
+    //我下的订单
+    public static final String BUY_LIST = "0";
+    //收到的订单
+    public static final String SELL_LIST = "1";
+
+}
