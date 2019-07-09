87209b2966ce60f5c918c7dda647127094b0fdf5
尘土 <1669691816@qq.com>
2019-07-09 15:25:46 +0800

    引入JWTUtils，apiKey错误待解决

--- src/main/java/top/dbon/blackandwhite/util/JwtUtils.java
+++ src/main/java/top/dbon/blackandwhite/util/JwtUtils.java
@@ -1,22 +1,74 @@
-//package top.dbon.blackandwhite.util;
-//
-//import io.jsonwebtoken.Jwts;
-//import io.jsonwebtoken.SignatureAlgorithm;
-//import io.jsonwebtoken.security.Keys;
-//import java.security.Key;
-//
-///**
-// * @ClassName JwtUtils
-// * @Descrcription jwt工具类，用来生成及解析token令牌
-// * @Author zzc
-// * @Date 2019/7/9 10:45
-// * @Version 1.0
-// */
-//public class JwtUtils {
-//
-//    public static String createJws() {
-//        Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
-//        return Jwts.builder().setSubject("Joe").signWith(key).compact();
-//    }
-//
-//}
+package top.dbon.blackandwhite.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
+import java.util.Date;
+
+/**
+ * @ClassName JwtUtils
+ * @Descrcription jwt工具类，用来生成及解析token令牌
+ * @Author zzc
+ * @Date 2019/7/9 10:45
+ * @Version 1.0
+ */
+public class JwtUtils {
+
+    /**
+     * 生成令牌
+     * @param id
+     * @param issuer
+     * @param subject
+     * @param ttlMillis
+     * @return
+     */
+    public static String createJWT(String id, String issuer, String subject, long ttlMillis) {
+
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256; //指定签名的时候使用的签名算法
+
+        long nowMillis = System.currentTimeMillis(); //生成JWT的时间
+        Date now = new Date(nowMillis);
+
+        //We will sign our JWT with our ApiKey secret
+        byte[] apiKeySecretBytes = null;
+//        apiKeySecretBytes = DatatypeConverter.parseBase64Binary(apiKey.getSecret());
+        Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
+
+        //Let's set the JWT Claims
+        JwtBuilder builder = Jwts.builder().setId(id)
+                .setIssuedAt(now)
+                .setSubject(subject)
+                .setIssuer(issuer)
+                .signWith(signatureAlgorithm, signingKey);
+
+        //if it has been specified, let's add the expiration
+        if (ttlMillis >= 0) {
+            long expMillis = nowMillis + ttlMillis;
+            Date exp = new Date(expMillis);
+            builder.setExpiration(exp);
+        }
+
+        //Builds the JWT and serializes it to a compact, URL-safe string
+        return builder.compact();
+    }
+
+    /**
+     * 解码和验证令牌
+     * @param jwt
+     */
+    public static void parseJWT(String jwt) {
+        //This line will throw an exception if it is not a signed JWS (as expected)
+        Claims claims = Jwts.parser()
+//                .setSigningKey(DatatypeConverter.parseBase64Binary(apiKey.getSecret()))
+                .parseClaimsJws(jwt).getBody();
+        System.out.println("ID: " + claims.getId());
+        System.out.println("Subject: " + claims.getSubject());
+        System.out.println("Issuer: " + claims.getIssuer());
+        System.out.println("Expiration: " + claims.getExpiration());
+    }
+
+}
