3ffa7a9a8f0d40235cbabd614d4136d6d987d39e
尘土 <1669691816@qq.com>
2019-07-10 13:46:07 +0800

    JWT密钥更换成符合HS256标准的256位字符

--- src/main/java/top/dbon/blackandwhite/common/Constant.java
+++ src/main/java/top/dbon/blackandwhite/common/Constant.java
@@ -14,6 +14,6 @@ public class Constant {
     //收到的订单
     public static final String SELL_LIST = "1";
     //JWT密钥
-    public static final String JWT_SECRET = "qwejhj123jkh1231230089ffsdfjh";
+    public static final String JWT_SECRET = "3lx[C1M1PS4a0#'7S{o^RKOpfSUg0cdX55t3v<v6^ED239M<t6zqa?^9~Ne(7.06.\"10`<~+'NJ74cFwod;W[O48j4T4T3}1Z36ef04n2F;42kz3o.N7hU1bubR<Ca+j3;E713D*45Gf0xX319\"E/2cE+*{s3d.zY/7#]b$ZaR4<0=5P04fBY~\"1/SNB[9S):018SGnLL;209f60#7}*t?j152B~4FUfJ$5\"BE3095|{+2Sd|[H25S8EA292pjZw";
 
 }
